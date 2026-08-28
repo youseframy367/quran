38b6597bde49235341e188a55c1d08e29f573e39
youseframy367 <yousseframy367@gmail.com>
2026-08-28 16:41:59 +0300

    Import database modul

---
+++ src/users/users.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get(':id')
+  async findById(@Param('id') id: string) {
+    return this.usersService.findById(Number(id));
+  }
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseModule } from '../database/database.module';
 import { User } from './entities/user.entity';
+import { UsersService } from './users.service';
+import {UsersController} from "./users.controller"
 
 @Module({
   imports: [
+    DatabaseModule,
     TypeOrmModule.forFeature([User]),
   ],
+  providers: [UsersService],
+  exports: [UsersService],
+  controllers: [UsersController],
+
 })
 export class UsersModule {}
